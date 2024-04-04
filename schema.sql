8e8cdde5155332924282c2820b4994b36f45f81c
Tanu Gupta <121037070+tanugupta30@users.noreply.github.com>
2024-04-04 14:10:02 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1013,5 +1013,5 @@ insert into Orders (CustomerID, OrderDate, ProductID, Quantity) values (1000, '2
 
 -- Write query to create the index that you will create to optimize a query like
 -- SELECT * FROM Orders WHERE CustomerID = {abc} AND OrderDate > '{abc}';
-create index idx_Orders on Orders (CustomerID,OrderDate);
+CREATE INDEX idx_CustomerID_OrderDate ON Orders (CustomerID, OrderDate);
 
